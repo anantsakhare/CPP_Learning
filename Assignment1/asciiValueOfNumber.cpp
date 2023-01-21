4f2946d4e770dea1f9ee3377a13ecfac10518214
anantsakhare <dataofficetask@gmail.com>
2023-01-21 15:14:49 +0530

    All question of Assignment

---
+++ Assignment1/asciiValueOfNumber.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    // Print the ASCII value of character ‘U’.
+
+    char letter; 
+    
+    cin>>letter;
+
+    cout<< "ASCII Value of Character "<<letter<< " is " << int(letter)<<endl;
+
+    return 0;
+}
